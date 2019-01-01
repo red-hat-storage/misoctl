f83d88a63470ff8631737140f8094529b523d48f
Ken Dreyer <kdreyer@redhat.com>
2018-12-31 21:39:29 -0500

    chacra: fix name_version() handling of bad NVR values
    Prior to this change, name_version() would never raise for any string,
    even if there were no underscores in the NVR, or more than one
    underscore.
    
    Refactor name_version() to check the resulting length from split(), and
    raise if it's not valid.

--- misoctl/chacra.py
+++ misoctl/chacra.py
@@ -15,12 +15,12 @@ def name_version(nvr):
 
     :raises: ValueError if this does not look like a valid package.
     """
-    try:
-        return nvr.split('_', 1)
-    except ValueError:
-        # Friendlier error message here:
-        err = '%s is not a valid package build N-V-R' % nvr
-        raise ValueError(err)
+    result = nvr.split('_')
+    if len(result) == 2:
+        return result
+    # Friendlier error message here:
+    err = '%s is not a valid package build N-V-R' % nvr
+    raise ValueError(err)
 
 
 def name_version_release(nvr):
