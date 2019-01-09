f0575f0e18109910b85e2930745f3153baed8d9e
Ken Dreyer <kdreyer@redhat.com>
2019-01-09 15:20:16 -0700

    setup.py: split args to pytest.main
    The latest versions of pytest (v4.1) require a list of strings here, not
    one single string.

--- setup.py
+++ setup.py
@@ -132,8 +132,8 @@ class PyTest(TestCommand):
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
-        errno = pytest.main('misoctl --flake8 ' +
-                            self.pytest_args)
+        args = 'misoctl --flake8 ' + self.pytest_args
+        errno = pytest.main(args.split())
         sys.exit(errno)
 
 
